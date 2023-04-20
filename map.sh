466776c67370ff5884e88f4d519546f1064fca2f
markziemann <mark.ziemann@gmail.com>
2023-04-20 14:02:04 +1000

    using gene name and sequences from nick

--- map.sh
+++ map.sh
@@ -1,6 +1,8 @@
 
 #!/bin/bash
 
+OLDREF=ref/Sarcophilus_harrisii.mSarHar1.11.cdna+ncrna.all.fa.idx
+REF=ref2/seq.fa.idx
 for FQZ1 in fastq/*R1_001.fastq.gz ; do
 
   FQZ2=$(echo $FQZ1 | sed 's#_R1_#_R2_#' )
@@ -10,7 +12,7 @@ for FQZ1 in fastq/*R1_001.fastq.gz ; do
   FQ2=$( echo $FQ1 | sed 's#pair1.fastq#pair2.fastq#' )
 
   kallisto quant \
-  -i ref/Sarcophilus_harrisii.mSarHar1.11.cdna+ncrna.all.fa.idx \
+  -i $REF \
   -o ${FQ1}_kal -t 16 $FQ1 $FQ2
 
   rm $FQ1 $FQ2
@@ -20,4 +22,4 @@ done
 
 for TSV in fastq/*/abundance.tsv ; do
   NAME=$(echo $TSV | cut -d '_' -f1) ; cut -f1,4 $TSV | sed 1d | sed "s#^#${NAME}\t#"
-done > 3col.tsv
+done | gzip > 3col.tsv.gz
